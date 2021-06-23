968bc8d4bcb3d7b7981acce709eb8eaf604b0d6a
Thejan13th <58652506+ranjanlamsal@users.noreply.github.com>
2021-06-23 16:35:23 +0545

    Add files via upload
    lab6

--- lab6/lab6_1.c
+++ lab6/lab6_1.c
@@ -1,6 +1,25 @@
+
+/** 
+  * File:    Ass6_1.c
+  * 
+  * Author:  Ranjan Lamsal
+  * Date:     23 june 2021
+  * Partner:  I worked alone  
+  * Course:   Computer Engineering 101
+  * 
+  * Summary of File: 
+  * 
+  *   This file contains program that takes two numbers as input and
+  *   gives the highest common factor of those two numbers
+  * 
+  * 
+  */ 
+
+
 #include <stdio.h>
 
 int gcd(int num1, int num2);
+
 int main()
 {
     int n1, n2;
@@ -12,6 +31,16 @@ int main()
     return 0;
 }
 
+
+/** 
+  * 
+  * The gcd() function gives the gcd of two numbers
+  * 
+  * Parameters   : two integers (num1 and num2) 
+  * 
+  * Return Value : an integer (gcd of num1 and num2) 
+  * 
+  */ 
 int gcd(int num1, int num2)
 {
     if (num2 != 0)
